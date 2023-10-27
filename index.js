2684d9fc64266cb5a3705411a3f0bbdb7b219874
David - Revo U - Seoul - Team 2 <davidwahyuds@gmail.com>
2023-10-27 08:29:13 +0700

    add responsive design and pop up form

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-function showAlert(){
-    alert("Thank you for Ordering! Please wait for 30 minutes we will contact you!")
+var modal = document.getElementById("form-section");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+function showAlert() {
+  alert(
+    "Thank you for Ordering! Please wait for 30 minutes we will contact you!"
+  );
+  modal.style.display = "none";
+}
+
+function openNav() {
+  document.getElementById("mcdSidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mcdSidenav").style.width = "0";
 }
